ce5b2e32aaed95344a8e260f952ec835545abe74
Mohd Luqman <mohdluqman1508@gmail.com>
2024-10-19 10:26:29 +0530

    forgtot

--- lib/presentation/email_app.dart
+++ lib/presentation/email_app.dart
@@ -10,148 +10,140 @@ class EmailApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return GestureDetector(
-      onTap: () {
-        FocusScopeNode currentFocus = FocusScope.of(context);
-        if (!currentFocus.hasPrimaryFocus) {
-          currentFocus.unfocus();
-        }
-      },
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-              onPressed: () {
-                ref.read(navigationProvider.notifier).goBack();
-              },
-              icon: const Icon(Icons.arrow_back, color: Colors.black)),
-          title: Row(
+        leading: IconButton(
+            onPressed: () {
+              ref.read(navigationProvider.notifier).goBack();
+            },
+            icon: const Icon(Icons.arrow_back, color: Colors.black)),
+        title: Row(
+          children: [
+            SizedBox(
+              width: ResponsiveHelper.width(55),
+            ),
+            Image.asset(
+              AssetsImages.appbar,
+              height: ResponsiveHelper.height(28),
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              width: ResponsiveHelper.width(10),
+            ),
+            Text('OSTELLO',
+                style: TextStyle(
+                    fontSize: ResponsiveHelper.fontSize(24),
+                    fontWeight: FontWeight.w700,
+                    color: const Color(0xff7D23E0),
+                    overflow: TextOverflow.ellipsis))
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: ResponsiveHelper.width(20),
+              vertical: ResponsiveHelper.height(25)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                width: ResponsiveHelper.width(55),
+                height: ResponsiveHelper.height(35),
               ),
-              Image.asset(
-                AssetsImages.appbar,
-                height: ResponsiveHelper.height(28),
-                fit: BoxFit.cover,
+              Center(
+                child: Image.asset(
+                  AssetsImages.emailapp,
+                  height: ResponsiveHelper.height(260),
+                  width: ResponsiveHelper.width(260),
+                  fit: BoxFit.fill,
+                ),
               ),
               SizedBox(
-                width: ResponsiveHelper.width(10),
+                height: ResponsiveHelper.height(50),
               ),
-              Text('OSTELLO',
+              Center(
+                child: Text(
+                  "Check your phone",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
                   style: TextStyle(
-                      fontSize: ResponsiveHelper.fontSize(24),
-                      fontWeight: FontWeight.w700,
-                      color: const Color(0xff7D23E0),
-                      overflow: TextOverflow.ellipsis))
-            ],
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: ResponsiveHelper.width(20),
-                vertical: ResponsiveHelper.height(25)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: ResponsiveHelper.height(35),
-                ),
-                Center(
-                  child: Image.asset(
-                    AssetsImages.emailapp,
-                    height: ResponsiveHelper.height(260),
-                    width: ResponsiveHelper.width(260),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                SizedBox(
-                  height: ResponsiveHelper.height(50),
-                ),
-                Center(
-                  child: Text(
-                    "Check your phone",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: ResponsiveHelper.fontSize(24),
-                      fontWeight: FontWeight.w800,
-                      color: const Color(0xff201926),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: ResponsiveHelper.height(8),
-                ),
-                Center(
-                  child: Text(
-                    "We’ve sent password recovery instructionto you via SMS",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: ResponsiveHelper.fontSize(14),
-                        fontWeight: FontWeight.w500,
-                        color: const Color(0xff7C7C7C)),
-                  ),
-                ),
-                SizedBox(
-                  height: ResponsiveHelper.height(32),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    ref.read(navigationProvider.notifier).goToOTPScreen();
-                  },
-                  child: Gradientbutton(
-                    txt: 'Open email app',
-                    fontsize: ResponsiveHelper.fontSize(16),
+                    fontSize: ResponsiveHelper.fontSize(24),
                     fontWeight: FontWeight.w800,
-                    color: const Color(0xffFFF6FF),
+                    color: const Color(0xff201926),
                   ),
                 ),
-                SizedBox(
-                  height: ResponsiveHelper.height(16),
+              ),
+              SizedBox(
+                height: ResponsiveHelper.height(8),
+              ),
+              Center(
+                child: Text(
+                  "We’ve sent password recovery instructionto you via SMS",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: ResponsiveHelper.fontSize(14),
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xff7C7C7C)),
                 ),
-                Center(
-                  child: Text(
-                    "Will do it later",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: ResponsiveHelper.fontSize(16),
-                      fontWeight: FontWeight.w800,
-                      color: const Color(0xff7D23E0),
-                    ),
+              ),
+              SizedBox(
+                height: ResponsiveHelper.height(32),
+              ),
+              GestureDetector(
+                onTap: () {
+                  ref.read(navigationProvider.notifier).goToOTPScreen();
+                },
+                child: Gradientbutton(
+                  txt: 'Open email app',
+                  fontsize: ResponsiveHelper.fontSize(16),
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xffFFF6FF),
+                ),
+              ),
+              SizedBox(
+                height: ResponsiveHelper.height(16),
+              ),
+              Center(
+                child: Text(
+                  "Will do it later",
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: ResponsiveHelper.fontSize(16),
+                    fontWeight: FontWeight.w800,
+                    color: const Color(0xff7D23E0),
                   ),
                 ),
-                SizedBox(
-                  height: ResponsiveHelper.height(16),
+              ),
+              SizedBox(
+                height: ResponsiveHelper.height(16),
+              ),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                      text: 'No instructions on your number? ',
+                      style: TextStyle(
+                        fontSize: ResponsiveHelper.fontSize(14),
+                        fontWeight: FontWeight.w800,
+                        color: const Color(0xff201926),
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'Resend',
+                          style: TextStyle(
+                            fontSize: ResponsiveHelper.fontSize(14),
+                            fontWeight: FontWeight.w800,
+                            color: const Color.fromARGB(255, 155, 61, 237),
+                          ),
+                        )
+                      ]),
                 ),
-                Center(
-                  child: RichText(
-                    text: TextSpan(
-                        text: 'No instructions on your number? ',
-                        style: TextStyle(
-                          fontSize: ResponsiveHelper.fontSize(14),
-                          fontWeight: FontWeight.w800,
-                          color: const Color(0xff201926),
-                        ),
-                        children: [
-                          TextSpan(
-                            text: 'Resend',
-                            style: TextStyle(
-                              fontSize: ResponsiveHelper.fontSize(14),
-                              fontWeight: FontWeight.w800,
-                              color: const Color.fromARGB(255, 155, 61, 237),
-                            ),
-                          )
-                        ]),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
